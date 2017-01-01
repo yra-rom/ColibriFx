6441f23e12b2ddc2e1ad87fca9c6e99c4b295030
plague <Ashaxpress295>
2017-01-01 21:53:30 +0200

    Images brightness effect.

--- src/gui/chat/ChatController.java
+++ src/gui/chat/ChatController.java
@@ -1,15 +1,26 @@
 package gui.chat;
 
 import gui.Controller;
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.util.Duration;
 
 public class ChatController implements Controller {
+    public static final String STYLE_ON_HOVER = "-fx-effect: dropshadow(gaussian, #9ba6a8, 15, 0.0001, 0.05, 0.05);";
     //private ClientThread thread = ClientThread.getInstance();
 
 //    @FXML
 //    private TextArea taMessage;
+
+    @FXML
+    private Label lNick;
 
     @FXML
     private ImageView ivCall;
@@ -17,23 +28,65 @@ public class ChatController implements Controller {
     private ImageView ivVideo;
     @FXML
     private ImageView ivFile;
+    @FXML
+    private ImageView ivSend;
+
 
     @FXML
     public void initialize() {
+        lNick.setText("Friend's Nick");
         initTA();
         initImageListeners();
         //thread.setController(this);
     }
 
     private void initImageListeners() {
-        ivCall.setOnMouseEntered(t -> ivCall.setStyle("-fx-effect: dropshadow(gaussian, #9ba6a8, 15, 0.0001, 0.05, 0.05);"));
-        ivCall.setOnMouseExited(t -> ivCall.setStyle(""));
+        ImageView ivs[] = {ivCall, ivVideo, ivFile, ivSend};
 
-        ivVideo.setOnMouseEntered(t -> ivVideo.setStyle("-fx-effect: dropshadow(gaussian, #9ba6a8, 15, 0.0001, 0.05, 0.05);"));
-        ivVideo.setOnMouseExited(t -> ivVideo.setStyle(""));
+        for(ImageView iv : ivs) {
+            ColorAdjust colorAdjust = new ColorAdjust();
+            colorAdjust.setBrightness(0.0);
 
-        ivFile.setOnMouseEntered(t -> ivFile.setStyle("-fx-effect: dropshadow(gaussian, #9ba6a8, 15, 0.0001, 0.05, 0.05);"));
-        ivFile.setOnMouseExited(t -> ivFile.setStyle(""));
+            iv.setEffect(colorAdjust);
+            iv.setOnMouseEntered(e -> {
+
+                Timeline fadeInTimeline = new Timeline(
+                        new KeyFrame(Duration.seconds(0),
+                                new KeyValue(colorAdjust.brightnessProperty(), colorAdjust.brightnessProperty().getValue(), Interpolator.LINEAR)),
+                        new KeyFrame(Duration.seconds(0.1), new KeyValue(colorAdjust.brightnessProperty(), -0.35, Interpolator.LINEAR)
+                        ));
+                fadeInTimeline.setCycleCount(1);
+                fadeInTimeline.setAutoReverse(false);
+                fadeInTimeline.play();
+
+            });
+
+            iv.setOnMouseExited(e -> {
+
+                Timeline fadeOutTimeline = new Timeline(
+                        new KeyFrame(Duration.seconds(0),
+                                new KeyValue(colorAdjust.brightnessProperty(), colorAdjust.brightnessProperty().getValue(), Interpolator.LINEAR)),
+                        new KeyFrame(Duration.seconds(0.7), new KeyValue(colorAdjust.brightnessProperty(), 0, Interpolator.LINEAR)
+                        ));
+                fadeOutTimeline.setCycleCount(1);
+                fadeOutTimeline.setAutoReverse(false);
+                fadeOutTimeline.play();
+
+            });
+        }
+
+
+//        ivCall.setOnMouseEntered(t -> ivCall.setStyle(STYLE_ON_HOVER));
+//        ivCall.setOnMouseExited(t -> ivCall.setStyle(""));
+//
+//        ivVideo.setOnMouseEntered(t -> ivVideo.setStyle(STYLE_ON_HOVER));
+//        ivVideo.setOnMouseExited(t -> ivVideo.setStyle(""));
+//
+//        ivFile.setOnMouseEntered(t -> ivFile.setStyle(STYLE_ON_HOVER));
+//        ivFile.setOnMouseExited(t -> ivFile.setStyle(""));
+//
+//        ivSend.setOnMouseEntered(t -> ivSend.setStyle(STYLE_ON_HOVER));
+//        ivSend.setOnMouseExited(t -> ivSend.setStyle(""));
     }
 
     private void initTA() {
